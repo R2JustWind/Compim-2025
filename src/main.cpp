fb73683a6b4e26f05cc10d1cd1facd4e6433ec0a
Arthur Soares Magalhães Freire <arthursmfreire@gmail.com>
2026-01-16 13:52:16 -0300

    Adição das funções para escolher qual direção o robô irá andar

--- src/main.cpp
+++ src/main.cpp
@@ -17,7 +17,14 @@
 #define RTD_PWM 11
 
 
-int setMotor(int dir1, int dir2, int pwm, int speed);
+void setMotor(int dir1, int dir2, int pwm, int speed);
+void moveForward(int speed);
+void moveBackward(int speed);
+void moveLeft(int speed);
+void moveRight(int speed);
+void turnLeft(int speed);
+void turnRight(int speed);
+void stop();
 
 void setup() {
   pinMode(RFE_DIR1, OUTPUT);
@@ -45,8 +52,7 @@ void loop() {
   delay(2000);
 }
 
-
-int setMotor(int dir1, int dir2, int pwm, int speed) {
+void setMotor(int dir1, int dir2, int pwm, int speed) {
   speed = constrain(speed, -255, 255);
 
   if (speed > 0) {
@@ -62,4 +68,53 @@ int setMotor(int dir1, int dir2, int pwm, int speed) {
     digitalWrite(dir2, LOW);
     analogWrite(pwm, 0);
   }
+}
+
+void moveForward(int speed) {
+  setMotor(RFE_DIR1, RFE_DIR2, RFE_PWM, speed);
+  setMotor(RFD_DIR1, RFD_DIR2, RFD_PWM, speed);
+  setMotor(RTE_DIR1, RTE_DIR2, RTE_PWM, speed);
+  setMotor(RTD_DIR1, RTD_DIR2, RTD_PWM, speed);
+}
+
+void moveBackward(int speed) {
+  setMotor(RFE_DIR1, RFE_DIR2, RFE_PWM, -speed);
+  setMotor(RFD_DIR1, RFD_DIR2, RFD_PWM, -speed);
+  setMotor(RTE_DIR1, RTE_DIR2, RTE_PWM, -speed);
+  setMotor(RTD_DIR1, RTD_DIR2, RTD_PWM, -speed);
+}
+
+void moveLeft(int speed) {
+  setMotor(RFE_DIR1, RFE_DIR2, RFE_PWM, -speed);
+  setMotor(RFD_DIR1, RFD_DIR2, RFD_PWM, speed);
+  setMotor(RTE_DIR1, RTE_DIR2, RTE_PWM, speed);
+  setMotor(RTD_DIR1, RTD_DIR2, RTD_PWM, -speed);
+}
+
+void moveRight(int speed) {
+  setMotor(RFE_DIR1, RFE_DIR2, RFE_PWM, speed);
+  setMotor(RFD_DIR1, RFD_DIR2, RFD_PWM, -speed);
+  setMotor(RTE_DIR1, RTE_DIR2, RTE_PWM, -speed);
+  setMotor(RTD_DIR1, RTD_DIR2, RTD_PWM, speed);
+}
+
+void turnLeft(int speed) {
+  setMotor(RFE_DIR1, RFE_DIR2, RFE_PWM, -speed);
+  setMotor(RFD_DIR1, RFD_DIR2, RFD_PWM, speed);
+  setMotor(RTE_DIR1, RTE_DIR2, RTE_PWM, -speed);
+  setMotor(RTD_DIR1, RTD_DIR2, RTD_PWM, speed);
+}
+
+void turnRight(int speed) {
+  setMotor(RFE_DIR1, RFE_DIR2, RFE_PWM, speed);
+  setMotor(RFD_DIR1, RFD_DIR2, RFD_PWM, -speed);
+  setMotor(RTE_DIR1, RTE_DIR2, RTE_PWM, speed);
+  setMotor(RTD_DIR1, RTD_DIR2, RTD_PWM, -speed);
+}
+
+void stop() {
+  setMotor(RFE_DIR1, RFE_DIR2, RFE_PWM, 0);
+  setMotor(RFD_DIR1, RFD_DIR2, RFD_PWM, 0);
+  setMotor(RTE_DIR1, RTE_DIR2, RTE_PWM, 0);
+  setMotor(RTD_DIR1, RTD_DIR2, RTD_PWM, 0);
 }
