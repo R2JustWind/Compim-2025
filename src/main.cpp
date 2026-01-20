3c3b0559e0bd7312f508a4e61ee2205d7ea5ead2
AnaKarolCampos <camposanakarol@gmail.com>
2026-01-20 11:33:31 -0300

    Correção das portas dos motores, correção da definição de direção no void setup, adição de loop para teste de movimentação e correção dos sentidos nas funções move

--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,24 @@
 #include <Arduino.h> 
 //DIR1 = INA  DIR2 = INB
-// Roda frontal esquerda
-#define RFE_DIR1 22
-#define RFE_DIR2 23
-#define RFE_PWM 5
-// Roda frontal direita
-#define RFD_DIR1 24
-#define RFD_DIR2 25
-#define RFD_PWM 6
-// Roda traseira esquerda
-#define RTE_DIR1 26
-#define RTE_DIR2 27
-#define RTE_PWM 9
-// Roda traseira direita
+// Driver1A - Roda frontal direita - M1
+#define RFD_DIR1 22
+#define RFD_DIR2 23
+#define RFD_PWM 4
+// Driver1B - Roda frontal esquerda - M2
+#define RFE_DIR1 24
+#define RFE_DIR2 25
+#define RFE_PWM 7
+// Driver2A - Roda traseira direita - M3
 #define RTD_DIR1 28
 #define RTD_DIR2 29
 #define RTD_PWM 10
+// Driver2B - Roda traseira esquerda - M4
+#define RTE_DIR1 26
+#define RTE_DIR2 27 
+#define RTE_PWM 13
 // Encoder roda frontal esquerda
 #define EFE_A 2
-#define EFE_B 3
+#define EFE_B 3  
 
 
 void setMotor(int dir1, int dir2, int pwm, int speed);
@@ -35,21 +35,34 @@ void setup() {
   pinMode(RFE_DIR2, OUTPUT);
   pinMode(RFE_PWM, OUTPUT);
 
-  pinMode(RFD_PWM, OUTPUT);
-  pinMode(RFD_PWM, OUTPUT);
+  pinMode(RFD_DIR1, OUTPUT);
+  pinMode(RFD_DIR2, OUTPUT);
   pinMode(RFD_PWM, OUTPUT);
 
-  pinMode(RTE_PWM, OUTPUT);
-  pinMode(RTE_PWM, OUTPUT);
+  pinMode(RTE_DIR1, OUTPUT);
+  pinMode(RTE_DIR2, OUTPUT);
   pinMode(RTE_PWM, OUTPUT);
 
-  pinMode(RTD_PWM, OUTPUT);
-  pinMode(RTD_PWM, OUTPUT);
+  pinMode(RTD_DIR1, OUTPUT);
+  pinMode(RTD_DIR2, OUTPUT);
   pinMode(RTD_PWM, OUTPUT);
 }
 
 void loop() {
-  
+  moveForward(50);
+  delay(3000);
+  moveBackward(50);
+  delay(3000);
+  moveLeft(50);
+  delay(3000);
+  moveRight(50);
+  delay(3000);
+  turnLeft(50);
+  delay(3000);
+  turnRight(50);
+  delay(3000);
+  stop();
+  delay(3000);
 }
 
 void setMotor(int dir1, int dir2, int pwm, int speed) {
@@ -70,46 +83,46 @@ void setMotor(int dir1, int dir2, int pwm, int speed) {
   }
 }
 
-void moveForward(int speed) {
-  setMotor(RFE_DIR1, RFE_DIR2, RFE_PWM, speed);
+void moveBackward(int speed) {
+  setMotor(RFE_DIR1, RFE_DIR2, RFE_PWM, -speed);
   setMotor(RFD_DIR1, RFD_DIR2, RFD_PWM, speed);
-  setMotor(RTE_DIR1, RTE_DIR2, RTE_PWM, speed);
+  setMotor(RTE_DIR1, RTE_DIR2, RTE_PWM, -speed);
   setMotor(RTD_DIR1, RTD_DIR2, RTD_PWM, speed);
 }
 
-void moveBackward(int speed) {
-  setMotor(RFE_DIR1, RFE_DIR2, RFE_PWM, -speed);
+void moveForward(int speed) {
+  setMotor(RFE_DIR1, RFE_DIR2, RFE_PWM, speed);
   setMotor(RFD_DIR1, RFD_DIR2, RFD_PWM, -speed);
-  setMotor(RTE_DIR1, RTE_DIR2, RTE_PWM, -speed);
+  setMotor(RTE_DIR1, RTE_DIR2, RTE_PWM, speed);
   setMotor(RTD_DIR1, RTD_DIR2, RTD_PWM, -speed);
 }
 
 void moveLeft(int speed) {
   setMotor(RFE_DIR1, RFE_DIR2, RFE_PWM, -speed);
-  setMotor(RFD_DIR1, RFD_DIR2, RFD_PWM, speed);
+  setMotor(RFD_DIR1, RFD_DIR2, RFD_PWM, -speed);
   setMotor(RTE_DIR1, RTE_DIR2, RTE_PWM, speed);
-  setMotor(RTD_DIR1, RTD_DIR2, RTD_PWM, -speed);
+  setMotor(RTD_DIR1, RTD_DIR2, RTD_PWM, speed);
 }
 
 void moveRight(int speed) {
   setMotor(RFE_DIR1, RFE_DIR2, RFE_PWM, speed);
-  setMotor(RFD_DIR1, RFD_DIR2, RFD_PWM, -speed);
+  setMotor(RFD_DIR1, RFD_DIR2, RFD_PWM, speed);
   setMotor(RTE_DIR1, RTE_DIR2, RTE_PWM, -speed);
-  setMotor(RTD_DIR1, RTD_DIR2, RTD_PWM, speed);
+  setMotor(RTD_DIR1, RTD_DIR2, RTD_PWM, -speed);
 }
 
 void turnLeft(int speed) {
   setMotor(RFE_DIR1, RFE_DIR2, RFE_PWM, -speed);
-  setMotor(RFD_DIR1, RFD_DIR2, RFD_PWM, speed);
+  setMotor(RFD_DIR1, RFD_DIR2, RFD_PWM, -speed);
   setMotor(RTE_DIR1, RTE_DIR2, RTE_PWM, -speed);
-  setMotor(RTD_DIR1, RTD_DIR2, RTD_PWM, speed);
+  setMotor(RTD_DIR1, RTD_DIR2, RTD_PWM, -speed);
 }
 
 void turnRight(int speed) {
   setMotor(RFE_DIR1, RFE_DIR2, RFE_PWM, speed);
-  setMotor(RFD_DIR1, RFD_DIR2, RFD_PWM, -speed);
+  setMotor(RFD_DIR1, RFD_DIR2, RFD_PWM, speed);
   setMotor(RTE_DIR1, RTE_DIR2, RTE_PWM, speed);
-  setMotor(RTD_DIR1, RTD_DIR2, RTD_PWM, -speed);
+  setMotor(RTD_DIR1, RTD_DIR2, RTD_PWM, speed);
 }
 
 void stop() {
