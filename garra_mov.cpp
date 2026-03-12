b7d0386f0ba86103eb36210f5ee491f8e14a4628
danielcm210 <danielcm2103@gmail.com>
2026-03-12 12:05:35 -0300

    movimentacao garra

---
+++ garra_mov.cpp
@@ -0,0 +1,105 @@
+#include <Arduino.h>
+#include <util/atomic.h>
+
+#define GARRA_DIR1 49
+#define GARRA_DIR2 48
+#define GARRA_PWM 12
+#define EGARRA_A 20
+#define EGARRA_B 35
+
+volatile int pulseCountGarra = 0;
+int primeiro_andar = 10000;
+int segundo_andar = 20000;
+int alvo = -30000;
+int altura = 0;
+int pulses_garra = 0;
+
+float deltaT;
+long currT;
+unsigned long lastTime = 0;
+
+int SobeGarra(int pulses);
+int DesceGarra(int pulses);
+void isrEGARRA();
+
+void setup() {
+Serial.begin(9600);
+
+  pinMode(GARRA_DIR1, OUTPUT);
+  pinMode(GARRA_DIR2, OUTPUT);
+  pinMode(GARRA_PWM, OUTPUT);
+
+  pinMode(EGARRA_A, INPUT_PULLUP);
+  pinMode(EGARRA_B, INPUT_PULLUP);
+
+  attachInterrupt(digitalPinToInterrupt(EGARRA_A), isrEGARRA, RISING);
+}
+
+void loop() {
+
+  altura = altura + pulses_garra;
+  SobeGarra(primeiro_andar - altura);
+  altura = altura + pulses_garra;
+  SobeGarra(segundo_andar - altura);
+  altura = altura + pulses_garra;
+  DesceGarra(alvo - altura);
+
+}
+
+int SobeGarra(int pulses) {
+  digitalWrite(GARRA_DIR1, HIGH);
+  digitalWrite(GARRA_DIR2, LOW);
+  analogWrite(GARRA_PWM, 255);
+
+  volatile int count = 0;
+  while (count < pulses){
+    currT = micros();
+    deltaT = ((float)(currT - lastTime)) / 1.0e6;
+
+    ATOMIC_BLOCK(ATOMIC_RESTORESTATE) {
+      count = - pulseCountGarra;
+    }
+
+    Serial.print(count);
+    Serial.println(' ');
+
+    lastTime = currT;
+  }
+
+  analogWrite(GARRA_PWM, 0);
+  pulses_garra = - count;
+  return count;
+}
+
+int DesceGarra(int pulses) {
+  digitalWrite(GARRA_DIR1, LOW);
+  digitalWrite(GARRA_DIR2, HIGH);
+  analogWrite(GARRA_PWM, 255);
+
+  volatile int count = 0;
+  while (count > pulses){
+    currT = micros();
+    deltaT = ((float)(currT - lastTime)) / 1.0e6;
+
+    ATOMIC_BLOCK(ATOMIC_RESTORESTATE) {
+      count = - pulseCountGarra;
+    }
+
+    Serial.print(count);
+    Serial.println(' ');
+
+    lastTime = currT;
+  }
+
+  analogWrite(GARRA_PWM, 0);
+  pulses_garra = - count;
+  return count;
+}
+
+void isrEGARRA() {
+  if(digitalRead(EGARRA_B) == HIGH) {
+    pulseCountGarra++;
+  } else {
+    pulseCountGarra--;
+  }
+}
